a5072443fc0d056e0186d5c2762f65b6c208d0fc
akhil <akhil@AkhiKrish.home>
2023-11-17 15:43:26 +0100

    testcase1 added

---
+++ HRM/src/testcases/TestCase1.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class TestCase1 {
+
+	public static void main(String[] args) 
+	
+	{
+		// TODO Auto-generated method stub
+		System.out.println("this is test case one");
+
+	}
+
+}
